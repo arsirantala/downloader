7989a2e583515adf950de41eae66830abcd465b5
arsirantala <arsi.rantala@kolumbus.fi>
2018-03-15 07:52:23 +0200

    Update make executable scripts. Add first versions of UI based and console based executables based on the python scripts

--- makeExecutable.bat
+++ makeExecutable.bat
@@ -1,3 +1,5 @@
 if exist build rmdir /s /q build
 if exist dist rmdir /s /q dist
-pyinstaller --noupx --onefile --hidden-import Tkinter -w DLer.py --version-file=version.txt
+pyinstaller --noupx --onefile --hidden-import Tkinter -w DLer.py --version-file=version.txt -n Downloader
+if not exist binaries mkdir binaries
+copy dist\*.exe binaries
--- makeExecutable.sh
+++
@@ -1,3 +0,0 @@
-rm -rf build &>/dev/null
-rm -rf dist &>/dev/null
-~/PycharmProjects/AnalyticTools/venv_2.7highsierra/bin/pyinstaller --onefile --hidden-import Tkinter -w DLer.py --version-file=version.txt
