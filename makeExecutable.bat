6f8b83a2bd979c5ee3346ed35f61c2cdb2e954e5
Arsi Rantala <arsi.rantala@suunto.com>
2018-03-25 22:37:20 +0300

    Fix in startup issue.. the Downloader.exe now wants the ico file in the same directory where its started. Need to find a solution for this (that the icon is embedded in the executable) otherwise the Installshield project needs to be updated so that the ico file is also installed to the installdir

--- makeExecutable.bat
+++ makeExecutable.bat
@@ -3,3 +3,6 @@ if exist dist rmdir /s /q dist
 pyinstaller --noupx --onefile -w --hidden-import=Tkinter --hidden-import=configparser --hidden-import=requests --hidden-import=pkgutil DLer.py --icon=Highwind.ico --version-file=version.txt -n Downloader
 if not exist binaries mkdir binaries
 copy dist\*.exe binaries
+copy Highwind.ico binaries
+copy Highwind.ico dist
+
